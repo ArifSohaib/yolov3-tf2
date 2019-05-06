19fbc0f3bd3d41d2cfc5822fe398869525a1b083
Sohaib <arif_sohaib@outlook.com>
2019-05-06 18:06:28 -0400

    added video output and early stopping when video ends

--- detect_video.py
+++ detect_video.py
@@ -17,6 +17,7 @@ flags.DEFINE_boolean('tiny', False, 'yolov3 or yolov3-tiny')
 flags.DEFINE_integer('size', 416, 'resize images to')
 flags.DEFINE_string('video', './data/video.mp4',
                     'path to video file or number for webcam)')
+flags.DEFINE_string('output_video', './data/video_output.mp4','path to where output video will be stored')
 
 
 def main(_argv):
@@ -35,15 +36,21 @@ def main(_argv):
 
     try:
         vid = cv2.VideoCapture(int(FLAGS.video))
+        vid.set(3, 3840)
+        vid.set(4, 2160)
+        vid.set(5, 10)
     except:
         vid = cv2.VideoCapture(FLAGS.video)
+    frame_width = int(vid.get(3))
+    frame_height = int(vid.get(4))
+    vid_out = cv2.VideoWriter(FLAGS.output_video, cv2.VideoWriter_fourcc('M','J','P','G'),30.0, (frame_width,frame_height))
+
     while True:
         _, img = vid.read()
-
         if img is None:
             logging.warning("Empty Frame")
             time.sleep(0.1)
-            continue
+            break
 
         img_in = tf.expand_dims(img, 0)
         img_in = transform_images(img_in, FLAGS.size)
@@ -56,13 +63,15 @@ def main(_argv):
 
         img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
         img = cv2.putText(img, "Time: {:.2f}ms".format(sum(times)/len(times)*1000), (0, 30),
-                          cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
+                          cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+        vid_out.write(img)                  
         cv2.imshow('output', img)
         if cv2.waitKey(1) == ord('q'):
             break
-
+    vid_out.release()
+    vid.release()
     cv2.destroyAllWindows()
-
+    
 
 if __name__ == '__main__':
     try:
